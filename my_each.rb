8c2fc95f666018a47ac064f63d939cbb02eecf70
confident-token-1019 <theoonetj@gmail.com>
2019-12-17 07:19:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,4 @@
 def my_each (array)# put argument(s) here
- if block_given?
   i = 0 
   while i < array. length 
   
@@ -8,11 +7,4 @@ def my_each (array)# put argument(s) here
   
 end
 array
-end
-  # code here
-  
-  collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
 end
